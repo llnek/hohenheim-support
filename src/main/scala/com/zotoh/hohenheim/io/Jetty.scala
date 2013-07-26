f76e0a085834eb38a928346220150b1588fc706c
Ken Leung <mailtokenl@gmail.com>
2013-07-25 21:30:49 -0700

    kl

---
+++ src/main/scala/com/zotoh/hohenheim/io/Jetty.scala
@@ -0,0 +1,43 @@
+/*??
+ * COPYRIGHT (C) 2012-2013 CHERIMOIA LLC. ALL RIGHTS RESERVED.
+ *
+ * THIS IS FREE SOFTWARE; YOU CAN REDISTRIBUTE IT AND/OR
+ * MODIFY IT UNDER THE TERMS OF THE APACHE LICENSE,
+ * VERSION 2.0 (THE "LICENSE").
+ *
+ * THIS LIBRARY IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
+ * BUT WITHOUT ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * SEE THE LICENSE FOR THE SPECIFIC LANGUAGE GOVERNING PERMISSIONS
+ * AND LIMITATIONS UNDER THE LICENSE.
+ *
+ * You should have received a copy of the Apache License
+ * along with this distribution; if not, you may obtain a copy of the
+ * License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ ??*/
+
+
+package com.zotoh.hohenheim.io
+
+import org.eclipse.jetty.webapp.WebAppContext
+
+/**
+ * @author kenl
+ */
+object JettyUtils {
+
+  def newWebAppContext( attr:String, obj:Any ) = {
+    new WebAppContext()  {
+      override def setContextPath(s:String) {
+        super.setContextPath(s)
+        _scontext.setAttribute(attr, obj)
+      }
+    }
+  }
+
+
+}
+
